135c48daf05dbffff7bdd650b33a4a0f479c040b
Vinod Kumar Reddy Peram <vinodkumarreddyperam@Vinods-MacBook-Air.local>
2020-06-29 16:37:43 -0500

    Implemented Checkered board

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+	state = {
+		boxes: 0
+	}
+	changeHandler = (event) => {
+		this.setState({boxes: event.target.value});
+	}
+	render() {
+		const checkBoxBlackStyle = {
+			backgroundColor: 'black',
+			width: '20px',
+			height: '20px'
+		};
+		const checkBoxWhiteStyle = {
+			backgroundColor: 'white',
+			width: '20px',
+			height: '20px'
+		};
+		let even = null;
+		let odd = null;
+		let rows = null;
+		if(parseInt(this.state.boxes) > 0) {
+			even = Array(parseInt(this.state.boxes)).fill().map((each, i) => {
+				if(i%2 ==0 ){
+					return <td style={checkBoxBlackStyle}></td>
+				}
+				else {
+					return <td style={checkBoxWhiteStyle}></td>;
+				}
+			})
+			odd = Array(parseInt(this.state.boxes)).fill().map((each, i) => {
+				if(i%2 ==0 ){
+					return <td style={checkBoxWhiteStyle}></td>
+				}
+				else {
+					return <td style={checkBoxBlackStyle}></td>
+				}
+			});
+			rows = Array(parseInt(this.state.boxes)).fill().map((each, i) => {
+				if(i%2 == 0) {
+					return <tr>{even}</tr>
+				}
+				else {
+					return <tr>{odd}</tr>
+				}
+			});
+		}
 
+		return (
+			<div style={{textAlign: 'center'}}>
+				<input type='number' value={this.state.boxes} onChange={(event) => this.changeHandler(event)} />
+				<table align='center'>
+					{rows}
+				</table>
+			</div>
+		);
+	}
+}
 export default App;
